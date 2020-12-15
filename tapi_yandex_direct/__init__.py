31755966f2e16bdbcfcf382847908c55355770b3
pmaksimov <vur21@ya.ru>
2020-12-15 12:59:09 +0300

    new version

--- tapi_yandex_direct/__init__.py
+++ tapi_yandex_direct/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Pavel Maksimov'
 __email__ = 'vur21@ya.com'
-__version__ = '2020.12.14'
+__version__ = '2020.12.15'
 
 
 from .tapi_yandex_direct import YandexDirect, GetTokenYandexDirect
